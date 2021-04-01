5c467e132cce3f43ebda12fa47a3d0f188730386
Wilson Coelho <wilsonsince1990@gmail.com>
2021-04-01 12:33:59 +0530

    updated unless api

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,28 +1,30 @@
-const expressJwt = require('express-jwt')
+const expressJwt = require("express-jwt");
 
 function authJwt() {
-    const secret = process.env.secret
-    return expressJwt({
-        secret,
-        algorithms:['HS256'],
-        isRevoked: isRevoked
-    }).unless({
-        path:[{url:/\/public\/uploads(.*)/, methods:['GET', 'OPTIONS']},
-            {url:/\/api\/v1\/products(.*)/, methods:['GET', 'OPTIONS']},
-        {url:/\/api\/v1\/categories(.*)/, methods:['GET', 'OPTIONS']},
-            '/api/v1/users/login',
-            '/api/v1/users/register',
-          
-        ]
-    })
+  const secret = process.env.secret;
+  return expressJwt({
+    secret,
+    algorithms: ["HS256"],
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+      { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+
+      "/api/v1/users/login",
+      "/api/v1/users/register",
+      "/api/v1/users/users",
+    ],
+  });
 }
 
-async function isRevoked(req, payload, done){
-    if(!payload.isAdmin){
-        done(null, true)
-    }
+async function isRevoked(req, payload, done) {
+  if (!payload.isAdmin) {
+    done(null, true);
+  }
 
-    done()
+  done();
 }
 
 module.exports = authJwt;
